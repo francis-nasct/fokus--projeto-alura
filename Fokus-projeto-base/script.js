667a7b87173fb8aaef14735627ee410d6d05efef
Francis <francivaldonascimentodosreis@gmail.com>
2026-06-06 22:21:57 -0300

    Add files via upload

---
+++ Fokus-projeto-base/script.js
@@ -0,0 +1,119 @@
+const html = document.querySelector('html')
+const focoBt = document.querySelector('.app__card-button--foco')
+const curtoBt = document.querySelector('.app__card-button--curto')
+const longoBt = document.querySelector('.app__card-button--longo')
+const banner = document.querySelector('.app__image')
+const titulo = document.querySelector('.app__title')
+const startPauseBt = document.querySelector('#start-pause')
+const botoes = document.querySelectorAll('.app__card-button')
+const musicaFocoInput = document.querySelector('#alternar-musica') //# pra id
+const iniciarOuPausarBt = document.querySelector('#start-pause span')
+const pauseBt = document.querySelector('.app__card-primary-butto-icon')
+const tempoNaTela = document.querySelector('#timer')
+const musica = new Audio('/sons/luna-rise-part-one.mp3')
+const audioBeep = new Audio('/sons/beep.mp3')
+const audioPlay = new Audio('/sons/play.wav')
+const audioPause = new Audio('/sons/pause.mp3')
+
+let tempoDecorridoEmSegundos = 5
+let intervaloId = null
+
+musica.loop = true
+
+musicaFocoInput.addEventListener('change', () => {
+    if(musica.paused){
+        musica.play()
+    }else{
+        musica.pause()
+    }
+})
+
+focoBt.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 5
+    alterarContexto('foco')
+    focoBt.classList.add('active')
+})
+
+curtoBt.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 300
+    alterarContexto('descanso-curto')
+    curtoBt.classList.add('active')
+})
+
+longoBt.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 900
+    alterarContexto('descanso-longo')
+    longoBt.classList.add('active')
+})
+
+function alterarContexto (contexto){
+    mostrarTempo()
+    botoes.forEach(function (contexto) {
+        contexto.classList.remove('active')
+    });
+    html.setAttribute('data-contexto', contexto)
+    banner.setAttribute('src', `/imagens/${contexto}.png`)
+    switch (contexto) {
+        case 'foco':
+            titulo.innerHTML =
+            `Otimize sua produtividade,<br>
+            <strong class="app__title-strong">mergulhe no que importa.</strong>`
+            break;
+        case 'descanso-curto':
+            titulo.innerHTML =
+            `Que tal dar uma respirada?<br>
+            <strong class="app__title-strong"> Faça uma pausa curta!</strong>`
+            break;
+        case 'descanso-longo':
+            titulo.innerHTML =
+            `Hora de voltar à superfície.<br>
+            <strong class="app__title-strong">Faça uma pausa longa.</strong>`
+        default:
+            break;
+    }
+}
+
+const contagemRegressiva = () => {
+    if(tempoDecorridoEmSegundos <= 0){
+        audioBeep.play()
+        alert('Tempo finalizado!')
+        const focoAtivo = html.getAttribute('data-contexto') == 'foco'
+        if(focoAtivo){
+            const elemento = new CustomEvent('focoFinalizado')
+            document.dispatchEvent(elemento) // é importante dispachar o elemento
+        }
+        zerar()
+        return
+    }
+    tempoDecorridoEmSegundos -= 1
+    mostrarTempo()
+}
+
+startPauseBt.addEventListener('click', iniciarOuPausar)
+
+function iniciarOuPausar (){
+    if(intervaloId){
+        audioPause.play()
+        zerar()
+        return
+    }
+    audioPlay.play()
+    intervaloId = setInterval(contagemRegressiva, 1000)
+    iniciarOuPausarBt.textContent = 'Pausar'
+    pauseBt.setAttribute('src', `/imagens/pause.png`)
+}
+
+function zerar (){
+    clearInterval(intervaloId)
+    iniciarOuPausarBt.textContent = 'Começar'
+    pauseBt.setAttribute('src', `/imagens/play_arrow.png`)
+    intervaloId = null
+}
+
+function mostrarTempo (){
+    const tempo = new Date(tempoDecorridoEmSegundos * 1000)
+    const tempoFormatado = tempo.toLocaleTimeString('pt-Br', {minute: '2-digit', second: '2-digit'})
+    tempoNaTela.innerHTML = `${tempoFormatado}`
+}
+
+mostrarTempo()
